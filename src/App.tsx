e8f04136c06d0d05199d28f368b6843eeebac640
Oleg Kashechkin <95638297+RavenDenster@users.noreply.github.com>
2023-04-26 14:28:57 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { observer } from 'mobx-react-lite';
+import { storeAnnotation } from 'mobx/dist/internal';
+import React, {FC, useContext, useEffect, useState} from 'react';
+import { Context } from '.';
+import LoginForm from './components/LoginForm';
+import { IUser } from './models/IUser';
+import UserService from './services/UserService';
+
+const App: FC = () => {
+  
+  const {store} = useContext(Context)
+  //@ts-ignore
+  const [users, setUsers] = useState<IUser>([])
+
+  useEffect(() => {
+    if(localStorage.getItem('token')) {
+      store.checkAuth()
+    }
+  }, [])
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers()
+      //@ts-ignore
+      setUsers(response.data)
+    } catch(e) {
+        console.log(e)
+    }
+  }
+
+  if(store.isLoading) {
+    return <div>Загрузка...</div>
+  }
+
+  if(!store.isAuth) {
+    return (
+      <LoginForm/>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : 'Авторивуйся'}</h1>
+      <h1>{store.user.isActivated ? 'Аккаунт подтвержден по почте' : 'Подтвердите аккаунт!!!'}</h1>
+      <button onClick={() => store.logout()}>Выйти</button>
+      <div>
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+      {/* @ts-ignore */}
+      {users.map(user =>
+          <div key={user.email}>{user.email}</div>
+        )}
     </div>
   );
 }
 
-export default App;
+export default observer(App);
